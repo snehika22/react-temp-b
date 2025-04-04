3074a1a7df5e532ff6c266a27f9d5cd7ba7ee38d
sneika22 <snhikab22@gmail.com>
2025-04-04 10:26:46 +0530

    add

--- src/components/App3.jsx
+++ src/components/App3.jsx
@@ -1,20 +1,34 @@
 import React from "react";
 import { useState } from "react";
-export default function App2() {
-  const [count, setCount] = useState(0);
-  const increment = () => {
-    setCount(count+1)
-    
-  };
-  const decrement = () => {
-    setCount(count-1)
+export default function App3() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [msg, setMsg] = useState();
+  const handleSubmit = () => {
+    if(email==="john@gmail.com" && password === "1234"){
+        setMsg("Welcome John!")
+    }
+    else {
+        setMsg("Access Denied")
+    }
   };
   return (
     <div>
-      <button onClick={decrement}>-</button>
-      {count}
-
-      <button onClick={increment}>+</button>
+      <h3>Login Form</h3>
+      {msg}
+      <p>
+        <input
+          type="text"
+          onChange={(event) => setEmail(event.target.value)}
+        ></input>
+      </p>
+      <p>
+        <input
+          type="password"
+          onChange={(event) => setPassword(event.target.value)}
+        ></input>
+      </p>
+      <button onClick={handleSubmit}>Log In</button>
     </div>
   );
 }
